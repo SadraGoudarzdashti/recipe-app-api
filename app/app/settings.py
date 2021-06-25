76dc76532ba42cff7163eb98f3f39883f935c1f5
sadra goudarzdashti <s.goudarzdashti@gmail.com>
2021-06-26 01:22:30 +0430

    add port no

--- app/app/settings.py
+++ app/app/settings.py
@@ -84,7 +84,8 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD')
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PORT': 5432,
     }
 }
 
